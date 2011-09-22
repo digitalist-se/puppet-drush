b0a68b48449f873cd02c6abf10f022c4552705fc
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2011-09-22 16:23:50 +0200

    Fixes warnings and stuff

--- manifests/definitions/drush_plugin.pp
+++ manifests/definitions/drush_plugin.pp
@@ -1,6 +1,6 @@
 define drush::plugin($plugin, $version, $destination = '/usr/share/drush/commands', $user = 'root') {
   exec { "install-drush-plugin-$plugin":
-    cwd => $root,
+    cwd => $destination,
     command => "/opt/drush/drush dl --destination=$destination $plugin-$version",
     creates => "$destination/$plugin",
     require => [Exec["drush install"]],

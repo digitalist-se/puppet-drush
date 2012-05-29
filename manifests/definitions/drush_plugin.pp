359943b22b057260b71bad75d7374544213ed958
Patrik Lembke <patrik.lembke@nodeone.se>
2012-05-29 12:05:53 +0200

    This should fix a very sever bounch of issues with apache, puppet and drush

--- manifests/definitions/drush_plugin.pp
+++ manifests/definitions/drush_plugin.pp
@@ -1,7 +1,7 @@
 define drush::plugin($plugin, $version, $destination = '/usr/share/drush/commands', $user = 'root') {
   exec { "install-drush-plugin-$plugin":
     cwd => $destination,
-    command => "/opt/drush/drush dl --destination=$destination $plugin-$version",
+    command => "/opt/drush/drush dl --destination=$destination ${plugin}-${version}",
     creates => "$destination/$plugin",
     require => [Exec["drush install"]],
     user => $user,

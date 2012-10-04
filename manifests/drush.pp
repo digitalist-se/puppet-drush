df31c53cd36e1e7e8893723abf9f38539dcf7948
Patrik Lembke <patrik.lembke@nodeone.se>
2012-10-04 11:26:47 +0200

    Vagrant MGMT, is at least installable now

--- manifests/drush.pp
+++ manifests/drush.pp
@@ -1,26 +1,10 @@
-class drush (
-  $tar = 'drush-7.x-4.5.tar.gz'
-) {
+class drush () {
   package { 'php5-cli':
     ensure => installed,
   }
 
-  exec { 'drush download':
-    cwd => '/root',
-    command => "/usr/bin/wget http://ftp.drupal.org/files/projects/${tar}",
-    creates => "/root/${tar}",
-    unless => '/usr/bin/test -d /opt/drush'
-  }
-
-  exec { 'drush install':
-    cwd => '/opt/',
-    command => "/bin/tar xvzf /root/${tar}; /opt/drush/drush",
-    creates => '/opt/drush',
-    unless => '/usr/bin/test -d /opt/drush',
-    require => [Exec['drush download'], Package['php5-cli']],
-  }
-
-  file { '/usr/local/bin/drush':
-    ensure => '/opt/drush/drush',
+  package { 'drush':
+    ensure => installed,
+    require => Package['php5-cli'],
   }
 }
